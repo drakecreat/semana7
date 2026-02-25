6838cb08dd4cb079a72e136c0ff35841514b5347
drakecreat <drake.creat@gmail.com>
2026-02-25 08:15:59 -0500

    input out

---
+++ io.cpp
@@ -0,0 +1,12 @@
+
+#include   <iostream>
+using namespace std;
+int main() {
+
+    cout << "Ingrese un número: ";
+    int num;
+    cin >> num;
+    cout << "El número ingresado es: " << num << endl;
+
+    return 0;
+}
